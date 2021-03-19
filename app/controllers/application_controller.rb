c9a10aeddde94ea446c06bc2b8405c91328b250d
Nathan Rymarz <nrymarz@gmail.com>
2021-03-19 17:57:09 -0500

    added .env to gitignore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,15 @@ class ApplicationController < ActionController::API
 
     def encode_token(payload)
         JWT.encode(payload,ENV['APP_SECRET'])
+    end
+
+    def decode_token(token)
+        if auth_header
+            begin
+                JWT.decode(token, ENV['APP_SECRET'])[0]
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
 end
