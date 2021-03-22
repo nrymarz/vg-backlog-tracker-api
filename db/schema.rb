e06a06e1b06d21fd3ae8f8def7c02832a0f740cf
Nathan Rymarz <nrymarz@gmail.com>
2021-03-22 00:42:18 -0500

    switched to postgres db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_20_055046) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
